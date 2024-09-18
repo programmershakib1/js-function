113c4471ce4137716d64853dae357b5534bf3faf
programmershakib <programmershakibinfo@gmail.com>
2024-09-19 00:26:40 +0600

    first commit

---
+++ conditional_return.js
@@ -0,0 +1,26 @@
+function isEven(number) {
+    if(number % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+// console.log(isEven(5));
+// console.log(isEven(110));
+
+const check = isEven(11);
+// console.log(check);
+
+
+function isOdd(number) {
+    if(number % 2 === 1){
+        return true
+    }
+    return false;
+}
+
+console.log(isOdd(10));
+console.log(isOdd(7));
---
+++ task.js
@@ -0,0 +1,74 @@
+// task 1
+// Take four parameters. Multiply the four numbers and then return the result
+
+
+// function multiply(num1, num2, num3, num4) {
+//     const result = num1 * num2 * num3 * num4;
+//     console.log(result);
+//     return result;
+// }
+
+// const total = multiply(2, 2, 2, 2);
+// console.log('total', total);
+
+
+// task 2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+
+// function oddEven(number) {
+//     if(number % 2 === 1){
+//         const odd = number * 2;
+//         console.log(odd);
+//         return odd;
+//     }
+//     else{
+//         const even = number / 2;
+//         console.log(even);
+//         return even;
+//     }
+// }
+
+// const result = oddEven(6);
+// console.log('result', result);
+
+
+// task 3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+
+// function make_avg(numbers) {
+//     let sum = 0;
+//     for(const number of numbers){
+//         console.log(number);
+//         sum = sum + number;
+//     }
+//     const avg = sum / numbers.length;
+//     return avg;
+// }
+
+// const avgs = make_avg([1, 2, 3, 4 , 5, 6, 7, 8, 9]);
+// console.log('avg', avgs);
+
+
+// task 4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+
+
+
+// task 5
+
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+// function odd_even(number) {
+//     if(number % 2 === 0){
+//         return ['Even'];
+//     }
+//     else{
+//         return ['Odd'];
+//     }
+// }
+
+// const result = odd_even(6);
+// console.log('result', result);
